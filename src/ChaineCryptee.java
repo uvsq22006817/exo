e7a918d99c62de3eb092cb41c0e65e6470e99dda
uvsq22006817 <naimabouchrit15@gmail.com>
2021-10-02 23:14:18 +0200

    ajout du constructeur

--- src/ChaineCryptee.java
+++ src/ChaineCryptee.java
@@ -1,4 +1,8 @@
 public class ChaineCryptee {
     private String enchiffre;
     private int decalage;
+    private ChaineCryptee (String enchiffre, int decalage) {
+        this.enchiffre=enchiffre;
+        this.decalage=decalage;
+    }
 }
