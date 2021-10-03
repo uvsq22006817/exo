0cedbcbac288f3fefafb20af44b079211cdc1b27
uvsq22006817 <naimabouchrit15@gmail.com>
2021-10-03 19:30:59 +0200

    ajout de la méthode String crypte() qui retourne la chaîne chiffrée plus quelques modifications

--- src/ChaineCryptee.java
+++ src/ChaineCryptee.java
@@ -8,7 +8,20 @@ public class ChaineCryptee {
     public String decrypte() {
 
 
+        if(this.enchiffre==null) return null;
         String s="";
+        int i;
+        for(i=0;i<this.enchiffre.length();i++) {
+            s+=(this.decaleCaractere(this.enchiffre.charAt(i),this.decalage));
+        }
+
         return s;
     }
+    public String crypte() {
+        return this.enchiffre;
+    }
+    private static char decaleCaractere(char c, int decalage) {
+        return (c < 'A' || c > 'Z')? c : (char)(((c - 'A' + decalage) % 26) + 'A');
+    }
+
 }
